f2f84fc65e5a3ce232a22450cb7554c3628d721e
Teo Dell'Amico <teo.dellamico@gmail.com>
2013-11-06 20:03:51 -0800

    fixed activities page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Mm::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'pages#home'
+ root 'pages#splash'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
