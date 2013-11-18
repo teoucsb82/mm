35114c98239b8073abb69cd5e691b5b8d39e529d
Teo Dell'Amico <teo.dellamico@gmail.com>
2013-11-17 18:22:37 -0800

    password 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_filter :authenticate, except: [:splash]
+  before_filter :authenticate
 
   def authenticate
     authenticate_or_request_with_http_basic('Administration') do |username, password|
